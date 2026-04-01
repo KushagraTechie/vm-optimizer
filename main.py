78e48ee204a7905f324b8e0ed72ad0b17b8e2118
Kushagralpu2024 <shivvindkumar9906@gmail.com>
2026-04-01 09:37:22 +0530

    added LRU algorithm

--- main.py
+++ main.py
@@ -18,3 +18,22 @@ pages = [7, 0, 1, 2, 0, 3, 0, 4]
 frames = 3
 
 print("FIFO Page Faults:", fifo(pages, frames))
+
+def lru(pages, frames):
+    memory = []
+    page_faults = 0
+
+    for page in pages:
+        if page not in memory:
+            if len(memory) < frames:
+                memory.append(page)
+            else:
+                memory.pop(0)
+                memory.append(page)
+            page_faults += 1
+        else:
+            memory.remove(page)
+            memory.append(page)
+
+    return page_faults
+print("LRU Page Faults:", lru(pages, frames))
